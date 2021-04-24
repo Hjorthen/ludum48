1cbc8b69b252a259e0080a10f90c35e3b57488eb
TheLazzoro <43842641+TheLazzoro@users.noreply.github.com>
2021-04-24 17:24:29 +0200

    ayy lmayo

---
+++ Assets/movement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public ParticleSystem bullet;
+    public ParticleSystem flamethrower;
+    private float bulletCooldown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * 0.01f;
+
+        if(Input.GetKey(KeyCode.B))
+        {
+            flamethrower.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F) && bulletCooldown <= 0) {
+            {
+                bullet.Play();
+                bulletCooldown = 1;
+            }
+
+        }
+
+        if (bulletCooldown > 0)
+        {
+            bulletCooldown -= Time.deltaTime;
+        }
+    }
+}
